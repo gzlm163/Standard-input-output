c74a2f0c00634937df53f45dfc1e57e34f83f045
gzlm163 <marusyagzl@gmail.com>
2026-03-18 21:45:56 +0700

    Search class added

---
+++ ConsoleApp4/FileSearch.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+public class FileSearch {
+  public List<string> Search(List<TextFile> files, string[] keywords) {
+    List<string> result = new List<string>();
+
+    for (int fileIndex = 0; fileIndex < files.Count; ++fileIndex) {
+      TextFile file = files[fileIndex];
+      file.ReadFromFile();
+
+      bool wordFound = false;
+
+      for (int wordIndex = 0; wordIndex < keywords.Length; ++wordIndex) {
+        if (file.fileContent.Contains(keywords[wordIndex])) {
+          wordFound = true;
+          break;
+        }
+      }
+
+      if (wordFound) {
+        string fullPath = file.filePath;
+        string[] pathParts = fullPath.Split('\\');
+        string fileName = pathParts[pathParts.Length - 1];
+        result.Add(fileName);
+      }
+    }
+
+    return result;
+  }
+}
---
+++ ConsoleApp4/TextFile.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+using System.Xml.Serialization;
+
+[Serializable]
+public class TextFile {
+  public string filePath;
+  public string fileContent;
+  public TextFile(string path) {
+    filePath = path;
+    fileContent = "";
+  }
+  public void ReadFromFile() {
+    try {
+      FileStream file = new FileStream(filePath, FileMode.OpenOrCreate, FileAccess.Read);
+      StreamReader reader = new StreamReader(file);
+      fileContent = reader.ReadToEnd();
+      reader.Close();
+    }
+    catch {
+      Console.WriteLine(" Error reading file. ");
+      fileContent = "";
+    }
+  }
+
+  public void SaveToFile() {
+    FileStream file = new FileStream(filePath, FileMode.OpenOrCreate, FileAccess.Write);
+    StreamWriter writer = new StreamWriter(file);
+    writer.Write(fileContent);
+    writer.Close();
+  }
+
+  public void BinarySerialize(FileStream fs) {
+    BinaryFormatter bf = new BinaryFormatter();
+    bf.Serialize(fs, this);
+    fs.Flush();
+    fs.Close();
+  }
+
+  public void BinaryDeserialize(FileStream fs) {
+    BinaryFormatter bf = new BinaryFormatter();
+    TextFile deserialized = (TextFile)bf.Deserialize(fs);
+    filePath = deserialized.filePath;
+    fileContent = deserialized.fileContent;
+    fs.Close();
+  }
+
+  public void XmlSerialize(FileStream fs) {
+    XmlSerializer xml = new XmlSerializer(this.GetType());
+    xml.Serialize(fs, this);
+    fs.Flush();
+    fs.Close();
+  }
+
+  public void XmlDeserialize(FileStream fs) {
+    XmlSerializer xml = new XmlSerializer(this.GetType());
+    TextFile deserialized = (TextFile)xml.Deserialize(fs);
+    filePath = deserialized.filePath;
+    fileContent = deserialized.fileContent;
+    fs.Close();
+  }
+}
